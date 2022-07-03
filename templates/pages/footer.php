a2674299e7fb5b9e5b6deaf3dc05034ba017ef9a
Lingga <linggaadiatmdj@gmail.com>
2022-07-03 21:43:20 +0700

    fixed typo

--- templates/pages/footer.php
+++ templates/pages/footer.php
@@ -2,7 +2,7 @@
 <footer class="page-footer font-small blue pt-4 mt-2">
   <!-- Copyright -->
   <div class="footer-copyright text-center py-4 bg-dark">© 2022 Copyright 
-    <span> PETSHOPQU</a>
+    <span> PETSHOPQU</span>
   </div>
   <!-- Copyright -->
 
